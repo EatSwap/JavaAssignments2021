102d532a013545d16bf953244f7a640a5695318b
Lam Haoyin <root@eatswap.org>
2021-12-12 17:05:21 +0800

    add: task 6

---
+++ src/ExperimentA/Task6/Animal.java
@@ -0,0 +1,21 @@
+package ExperimentA.Task6;
+
+/**
+ * Author: Lam Haoyin
+ * GPG: C00E4E4FCC31CDF3
+ * Date: 16:39 12/12/21
+ * Project: JavaAssignments2021
+ */
+
+public abstract class Animal {
+	private final String name;
+
+	public Animal(String theName) {
+		name = theName;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
+
---
+++ src/ExperimentA/Task6/Duck.java
@@ -0,0 +1,16 @@
+package ExperimentA.Task6;
+
+/**
+ * Author: Lam Haoyin
+ * GPG: C00E4E4FCC31CDF3
+ * Date: 17:00 12/12/21
+ * Project: JavaAssignments2021
+ */
+
+public class Duck extends Animal {
+	int size;
+	public Duck(int newSize, String newName) {
+		super(newName);
+		size = newSize;
+	}
+}
